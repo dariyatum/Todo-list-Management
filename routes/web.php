c60ae94c6af3b4b681be198a93724d24d3b5c6d7
dariyatum <dariyatum412@gmail.com>
2026-05-08 09:50:40 +0700

    get

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
-Route::get('/mytasks', function () {
-    return view('mytasks.index');
-})->name('mytasks');
+Route::get('/tasks', function () {
+    return view('tasks.index');
+})->name('tasks');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('tasks/show', [TaskController::class, 'show'])->name('tasks.show');
+    Route::post('/tasks/store', [TaskController::class, 'store'])->name('tasks.store');
